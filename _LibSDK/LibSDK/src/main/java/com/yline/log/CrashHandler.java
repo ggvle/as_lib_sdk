6fda0bcea9fc630c65b1ce87ce9f435d42dae90d
yline <957339173@qq.com>
2017-02-14 13:59:11 +0800

    this:update next:continue

--- _LibSDK/LibSDK/src/main/java/com/yline/log/CrashHandler.java
+++ _LibSDK/LibSDK/src/main/java/com/yline/log/CrashHandler.java
@@ -120,7 +120,7 @@ public final class CrashHandler implements UncaughtExceptionHandler
 		
 		// 收集设备参数信息
 		collectDeviceInfo(mApplication);
-		
+
 		// 保存日志文件
 		saveCrashInfo2File(ex);
 
@@ -167,7 +167,7 @@ public final class CrashHandler implements UncaughtExceptionHandler
 		}
 		catch (NameNotFoundException e)
 		{
-			LogUtil.e(TAG + " collectDeviceInfo -> NameNotFoundException error happened");
+			LogUtil.e(TAG + " collectDeviceInfo -> NameNotFoundException error happened", e);
 		}
 		
 		// 记录设备信息
@@ -182,11 +182,11 @@ public final class CrashHandler implements UncaughtExceptionHandler
 			}
 			catch (IllegalArgumentException e)
 			{
-				LogUtil.e(TAG + " getDeclaredFields -> IllegalArgumentException  error happened");
+				LogUtil.e(TAG + " getDeclaredFields -> IllegalArgumentException  error happened", e);
 			}
 			catch (IllegalAccessException e)
 			{
-				LogUtil.e(TAG + " getDeclaredFields -> IllegalAccessException  error happened");
+				LogUtil.e(TAG + " getDeclaredFields -> IllegalAccessException  error happened", e);
 			}
 		}
 	}
